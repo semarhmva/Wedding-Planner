409fadc696e396db9615230cf2c351fc741ca517
semarhmva <semarhmva2003@gmail.com>
2024-09-02 20:26:24 +0400

    services

--- src/components/details/ClientRequest.jsx
+++ src/components/details/ClientRequest.jsx
@@ -5,7 +5,7 @@ function ClientRequest() {
   return (
      <div className="client">
         <div className="container">
-          <div className="client-left">
+          {/* <div className="client-left">
             <div className="img">
                 <img src="/services1.png" alt="" />
             </div>
@@ -13,7 +13,29 @@ function ClientRequest() {
           <div className="client-right">
             <div className="title">
                 <TitleSection 
-                 
+                 subtitle="client request" 
+                 vectorLeft="/Vector 3.svg" 
+                 vectorRight="/Vector 1.svg" 
+               />
+            </div>
+            <div class="wedding-details">
+                <span>Our can help couples determine their wedding theme or concept and create suitable decoration and invitation designs.</span>
+  <ul>
+    <li><strong>Wedding date:</strong> <span class="highlight">March 4, 2023</span></li>
+    <li><strong>Budget:</strong> <span class="highlight">$ 2,999.00 USD</span></li>
+    <li><strong>Location:</strong> <span class="highlight">Milan, Italy</span></li>
+    <li><strong>Guest count:</strong> <span class="highlight">200 guest</span></li>
+    <li><strong>Theme and decoration:</strong> <span class="highlight">Elegant</span></li>
+    <li><strong>Additional services:</strong> <span class="highlight">Photography, Videography, Music</span></li>
+  </ul>
+</div>
+          </div> */}
+          <div className="c-left">
+            <img src="/services1.png" alt="" />
+          </div>
+          <div className="c-right">
+          <div className="title">
+                <TitleSection 
                  subtitle="client request" 
                  vectorLeft="/Vector 3.svg" 
                  vectorRight="/Vector 1.svg" 
--- src/components/home/HomeServices.jsx
+++ src/components/home/HomeServices.jsx
@@ -1,39 +1,46 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../../styles/HomeServices.css';
 import TitleSection from '../TitleSection';
 import Card from '../Card';
+
 function HomeServices() {
+    const navigate = useNavigate();
     const cardsData = [
-        { image: "/services1.png", altText: "Service 1", overlayText: "Design" },
-        { image: "/services2.png", altText: "Service 2", overlayText: "Planning" },
-        { image: "/services3.png", altText: "Service 3", overlayText: "Photography" },
-       
-      ];
-  return (
-    <div className="home-services">
-        <div className="container">
-            <div className="services-title">
-            <TitleSection 
-             subtitle="Services" 
-             mainTitle="We are committed to providing the best service"
-             vectorLeft="/Vector 3.svg" 
-             vectorRight="/Vector 1.svg" 
-            />
-            </div>
-            <div className="cards">
-            {cardsData.map((card, index) => (
-              <Card
-                 key={index}
-                 image={card.image}
-                 altText={card.altText}
-                 overlayText={card.overlayText}
-              />
-      ))}
-      
+        { image: "/services1.png", altText: "Service 1", overlayText: "Design", path: "/services" },
+        { image: "/services2.png", altText: "Service 2", overlayText: "Planning", path: "/services" },
+        { image: "/services3.png", altText: "Service 3", overlayText: "Photography", path: "/services" },
+    ];
+
+    const handleCardClick = (path) => {
+        navigate(path);
+    };
+
+    return (
+        <div className="home-services">
+            <div className="container">
+                <div className="services-title">
+                    <TitleSection 
+                        subtitle="Services" 
+                        mainTitle="We are committed to providing the best service"
+                        vectorLeft="/Vector 3.svg" 
+                        vectorRight="/Vector 1.svg" 
+                    />
+                </div>
+                <div className="cards">
+                    {cardsData.map((card, index) => (
+                        <Card
+                            key={index}
+                            image={card.image}
+                            altText={card.altText}
+                            overlayText={card.overlayText}
+                            onClick={() => handleCardClick(card.path)}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-  )
+    );
 }
 
 export default HomeServices;
