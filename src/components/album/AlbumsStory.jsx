a8ad28326ba178e5df2901c9d61b116c8998be5d
semarhmva <semarhmva2003@gmail.com>
2024-09-06 20:27:44 +0400

    .

--- src/components/album/AlbumsStory.jsx
+++ src/components/album/AlbumsStory.jsx
@@ -4,42 +4,41 @@ import TitleSection from '../TitleSection'
 
 function AlbumsStory() {
   return (
-    <div className="albums">
-<div className="container">
-
-  <div className="client-right">
-    <div className="title">
-    <TitleSection 
-                 mainTitle='The Full Story'
-                 subtitle="ALBUMS" 
+<div className="album">
+          
+        <div className="container">
+          <div className="client-left">
+            <div className="img">
+                <img src="/services3.png" alt="" />
+            </div>
+          </div>
+          <div className="client-right">
+            <div className="title">
+                <TitleSection 
+                 subtitle="The Highlight"
                  vectorLeft="/Vector 3.svg" 
                  vectorRight="/Vector 1.svg" 
                />
-    </div>
-    <div class="wedding-details">
-    
-  <p>
-  The Main offers a 8"X8" spread that is the perfect  on your coffee table whenever you're in the mood to relive your favorite moments.
-  </p>
-  <h2>SIZE</h2>
-  <p>
-  The Main offers a 8"X8" spread that is the perfect size to sit on whenever you're in the mood to relive your favorite moments.
-  </p>
-  <h2>SIZE</h2>
-  <p>
-  The Main offers a 8"X8" spread that is the perfect size to sit on your coffee table whenever your coffee table  .
-  </p>
+            </div>
+            <div class="wedding-details">
+            <h2>SIZE</h2>
+          <p>
+          The Main offers a 8"X8" spread that is  perfect  on your table whenever you're in the mood to relive your favorite moments.
+          </p>
+          <h2>SIZE</h2>
+          <p>
+          The Main offers a 8"X8" spread that is the perfect size to sit on whenever you're in the mood to relive your favorite moments.
+          </p>
+          <h2>SIZE</h2>
+          <p>
+          The Main offers a 8"X8" spread that is the perfect size to sit on your coffee table whenever your coffee table  .
+          </p>
 
 
 </div>
-  </div>
-  <div className="client-left">
-    <div className="img">
-        <img src="/services2.png" alt="" />
-    </div>
-  </div>
-</div>
-</div>
+          </div>
+        </div>
+     </div>
   )
 }
 
